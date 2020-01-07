a89483b088d5566d4e889d01d5e7ceb3573b073f
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-01-07 23:54:35 +0500

    Добавляет Vue инстанс, несколько свойств и заголовки для методов

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 new Vue({
   el: `#app`,
   data: {
-    
+    gameStarted: false,
+    healthbar: {
+      player: 100,
+      monster: 100
+    },
+    logMessages: []
+  },
+  computed: {
+
+  },
+  methods: {
+    startNewGame() {
+      //...
+    },
+    attack() {
+      //...
+    },
+    specialAttack() {
+      //...
+    },
+    heal() {
+      //...
+    },
+    giveUp() {
+      //...
+    },
+    addLogMessage() {
+      //...
+    }
   }
 });
