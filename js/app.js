2c625718836cd1460a1df3e2116625b376c27cb6
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-01-08 14:48:48 +0500

    Рефакторинг кода

--- js/app.js
+++ js/app.js
@@ -4,10 +4,19 @@ new Vue({
     gameStarted: false,
     playerHealth: 100,
     monsterHealth: 100,
+    player: {
+      maxDamage: 10,
+      minDamage: 3,
+      maxSpecDamage: 20,
+      minSpecDamage: 10
+    },
+    monster: {
+      maxDamage: 12,
+      minDamage: 5
+    },
+    winMessage: `Вы выиграли! Сыграем ещё?`,
+    lostMessage: `Увы, монстр оказался сильнее... Наваляем ему?`,
     logMessages: []
-  },
-  computed: {
-
   },
   methods: {
     startNewGame() {
@@ -15,11 +24,45 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+    calcDamage(minDamage, maxDamage) {
+      return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+    },
+    checkWin() {
+      if (this.monsterHealth <= 0) {
+        if (confirm(this.winMessage)) {
+          this.startNewGame();
+        } else {
+          this.gameStarted = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm(this.lostMessage)) {
+          this.startNewGame();
+        } else {
+          this.gameStarted = false;
+        }
+        return true;
+      }
+      return false;
+    },
     attack() {
-      //...
+      // Сделать рефакторинг с учётом спец. атаки
+      this.monsterHealth -= this.calcDamage(this.player.minDamage, this.player.maxDamage);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calcDamage(this.monster.minDamage, this.monster.maxDamage);
+      this.checkWin();
     },
     specialAttack() {
-      //...
+      this.monsterHealth -= this.calcDamage(this.player.minSpecDamage, this.player.maxSpecDamage);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calcDamage(this.monster.minDamage, this.monster.maxDamage);
+      this.checkWin();
     },
     heal() {
       //...
@@ -30,5 +73,6 @@ new Vue({
     addLogMessage() {
       //...
     }
-  }
+  },
+  computed: {}
 });
